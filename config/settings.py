55ec110c9fc3a4a5f70a0e67ff0fe918f84d717f
grrr127 <qrrr127@gmail.com>
2025-12-22 19:13:00 +0900

    🐛fix : settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST", default="127.0.0.1"),
+        "HOST": env("DB_HOST", default="db"),
         "PORT": env("DB_PORT", default="5432"),
     }
 }
